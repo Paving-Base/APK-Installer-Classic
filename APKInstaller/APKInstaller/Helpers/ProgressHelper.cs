f904bb7e6008034d157585bdc632e2c8ec067c68
wherewhere <qq2518766683@outlook.com>
2022-05-19 22:36:53 +0800

    修改了一些内容

--- APKInstaller/APKInstaller/Helpers/ProgressHelper.cs
+++ APKInstaller/APKInstaller/Helpers/ProgressHelper.cs
@@ -1,8 +1,8 @@
-﻿using APKInstaller.Win32;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Threading;
+using static APKInstaller.Interop.ShObjIdl;
 
 namespace APKInstaller.Helpers
 {
@@ -14,21 +14,21 @@ namespace APKInstaller.Helpers
     /// </summary>
     public static class ProgressHelper
     {
-        private static readonly ShobjidlCore.ITaskbarList? _taskbarList;
+        private static ITaskbarList4? _taskbarList;
 
         static ProgressHelper()
         {
-            if (!IsSupported()) { return; }
+            if (!IsSupported())
+                throw new Exception("Taskbar functions not available");
 
-            _taskbarList = new ShobjidlCore.CTaskbarList() as ShobjidlCore.ITaskbarList;
+            _taskbarList = new CTaskbarList() as ITaskbarList4;
 
             _taskbarList?.HrInit();
         }
 
         private static bool IsSupported()
         {
-            return Environment.OSVersion.Platform == PlatformID.Win32NT &&
-                Environment.OSVersion.Version.CompareTo(new Version(6, 1)) >= 0;
+            return OSVersionHelper.IsWindows7OrGreater;
         }
 
         /// <summary>
@@ -75,8 +75,7 @@ namespace APKInstaller.Helpers
 
         private static void SetProgressState(ProgressState state)
         {
-            // Application.Current.MainWindow.TaskbarItemInfo.ProgressState = (System.Windows.Shell.TaskbarItemProgressState) state;
-            _taskbarList?.SetProgressState(GetHandle(), state);
+            _taskbarList?.SetProgressState(GetHandle(), (TBPFLAG)Enum.Parse(typeof(TBPFLAG), ((int)state).ToString()));
         }
 
         private static void SetProgressValue(int current, int max)
@@ -90,9 +89,7 @@ namespace APKInstaller.Helpers
         private static IntPtr GetHandle()
         {
             if (Application.Current.MainWindow != null)
-            {
                 return new WindowInteropHelper(Application.Current.MainWindow).Handle;
-            }
 
             return IntPtr.Zero;
         }
